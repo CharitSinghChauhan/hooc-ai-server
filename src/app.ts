d64043535d2835688ed2808a5c87e314257be96c
CharitSinghChauhan <charitsinghchauhan@gmail.com>
2026-01-17 03:48:53 +0530

    cors error fix

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,8 @@ const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
