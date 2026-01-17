2aecc05c1aafe4a9d6d4948118d2516a8484ec65
CharitSinghChauhan <charitsinghchauhan@gmail.com>
2026-01-17 10:21:55 +0530

    fix(auth): MongoDB connection issues on Vercel

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,16 +1,31 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  const mongoose_uri = process.env.MONGODB_URI!;
+mongoose.set("bufferCommands", false);
 
-  try {
-    if (!mongoose_uri) throw new Error();
-    await mongoose.connect(mongoose_uri, {
-      dbName: "hooc-ai-db",
-    });
-    console.log(`MongoDB Connected`);
-  } catch (error) {
-    console.log("MongoDB connection error", error);
-    process.exit(1);
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI not defined");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
